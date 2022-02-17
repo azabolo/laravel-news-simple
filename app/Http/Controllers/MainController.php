aab7a8fd7b188f241d0acd5d8a64900480caddef
anton <zabolots@yandex.ru>
2022-02-17 20:31:39 +0700

    Добавлены комментарии в контроллер.

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -8,19 +8,30 @@ use Illuminate\Http\Request;
 
 class MainController extends Controller
 {
+    /**
+     * Первая страница сайта
+     *
+     * @param Request $request
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View
+     */
     public function index(Request $request)
     {
+        // id категории (get-параметр)
         $categoryId = $request->get('category');
 
+        // Требуется получить страницы с их категориями
         $pages = Page::with('category');
 
+        // Если категория указана - ограничим выборку страниц
         if ($categoryId) {
             $pages = $pages->whereCategoryId($categoryId);
         }
 
+        // Запросим страницы из БД
         $pages = $pages->orderByDesc('datetime_post')
             ->get();
 
+        // Все категории для поля формы с предшествующей опцией для выбора всех новостей
         $categories = Category::all(['id', 'name'])
             ->prepend(['id' => 0, 'name' => '- выберите -'])
             ->pluck('name', 'id');
@@ -32,8 +43,15 @@ class MainController extends Controller
         ]);
     }
 
+    /**
+     * Отдельная страница (новость)
+     *
+     * @param $pageId
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View
+     */
     public function page($pageId)
     {
+        // Страница или 404, если такой нет
         $page = Page::findOrFail($pageId);
 
         return view('page', ['page' => $page]);
